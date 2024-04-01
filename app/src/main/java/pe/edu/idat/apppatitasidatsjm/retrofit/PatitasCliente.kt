d431a04efef1de8e82c3895147ba782280a3fcce
aleibwyn <mf-a@outlook.com>
2024-04-01 13:31:02 -0500

    Mapeo de Objetos Api Rest 2

--- app/src/main/java/pe/edu/idat/apppatitasidatsjm/retrofit/PatitasCliente.kt
+++ app/src/main/java/pe/edu/idat/apppatitasidatsjm/retrofit/PatitasCliente.kt
@@ -1,4 +1,23 @@
 package pe.edu.idat.apppatitasidatsjm.retrofit
 
-class PatitasCliente {
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+
+object PatitasCliente {
+    private var retrofitCliente = OkHttpClient.Builder()
+        .connectTimeout(2, TimeUnit.MINUTES)
+        .readTimeout(30, TimeUnit.MINUTES)
+        .writeTimeout(15, TimeUnit.MINUTES)
+        .build()
+    private fun buildRetrofit() = Retrofit.Builder()
+        .baseUrl("http://www.kreapps.biz/patitas/")
+        .client(retrofitCliente)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+
+    val retrofitService: PatitasServicio by lazy {
+        buildRetrofit().create(PatitasServicio::class.java)
+    }
 }
--- app/src/main/java/pe/edu/idat/apppatitasidatsjm/retrofit/PatitasServicio.kt
+++ app/src/main/java/pe/edu/idat/apppatitasidatsjm/retrofit/PatitasServicio.kt
@@ -1,4 +1,25 @@
 package pe.edu.idat.apppatitasidatsjm.retrofit
 
+import pe.edu.idat.apppatitasidatsjm.retrofit.request.LoginRequest
+import pe.edu.idat.apppatitasidatsjm.retrofit.request.RegistroRequest
+import pe.edu.idat.apppatitasidatsjm.retrofit.request.VoluntarioRequest
+import pe.edu.idat.apppatitasidatsjm.retrofit.response.LoginResponse
+import pe.edu.idat.apppatitasidatsjm.retrofit.response.MascotaResponse
+import pe.edu.idat.apppatitasidatsjm.retrofit.response.RegistroResponse
+import retrofit2.Call
+import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.POST
+import retrofit2.http.PUT
+
 interface PatitasServicio {
+    @POST("login.php")
+    fun login(@Body loginRequest: LoginRequest): Call<LoginResponse>
+    @PUT("persona.php")
+    fun registro(@Body registroRequest: RegistroRequest): Call<RegistroResponse>
+    @POST("personavoluntaria.php")
+    fun voluntario(@Body voluntarioRequest: VoluntarioRequest): Call<RegistroResponse>
+    @GET("mascotaperdida.php")
+    fun mascotas(): Call<List<MascotaResponse>>
+
 }
